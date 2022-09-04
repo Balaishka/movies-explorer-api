770a0f32eb7fc4d43e52954fd3737ce4507f995c
Balai Mikheeva <balaishka@yandex.ru>
2022-09-04 19:11:15 +0300

    feat: check-list

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { errorsHandler } = require('./middlewares/errorsHandler');
 const { limiter } = require('./middlewares/rateLimiter');
 
-const { PORT = 3005 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(helmet());
